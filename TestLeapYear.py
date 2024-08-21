e2c1a31f200d896786dbba1ae78f8e36d801156e
steve.bement <steve.bement@accenture.com>
2024-08-21 11:09:54 -0600

    Converting to parameterized tests

--- TestLeapYear.py
+++ TestLeapYear.py
@@ -1,21 +1,27 @@
+import pytest
+
 import LeapYearChecker
 
 
 class TestAYearIsALeapYear:
     checker = LeapYearChecker
 
-    def test_if_it_is_divisible_by_4_but_not_by_100(self):
-        assert TestAYearIsALeapYear.checker.is_leap_year(1996) == True
+    @pytest.mark.parametrize("year", [2020, 2016, 1984, 4])
+    def test_if_it_is_divisible_by_4_but_not_by_100(self, year):
+        assert TestAYearIsALeapYear.checker.is_leap_year(year) == True
 
-    def test_if_it_is_divisible_by_400(self):
-        assert TestAYearIsALeapYear.checker.is_leap_year(1600) == True
+    @pytest.mark.parametrize("year", [400, 1600, 4000])
+    def test_if_it_is_divisible_by_400(self, year):
+        assert TestAYearIsALeapYear.checker.is_leap_year(year) == True
 
 
 class TestALeapYearIsNotALeapYear:
     checker = LeapYearChecker
 
-    def test_if_it_is_not_divisible_by_4(self):
-        assert TestAYearIsALeapYear.checker.is_leap_year(1995) == False
+    @pytest.mark.parametrize("year", [2022, 2019, 1999, 1])
+    def test_if_it_is_not_divisible_by_4(self, year):
+        assert TestAYearIsALeapYear.checker.is_leap_year(year) == False
 
-    def test_if_it_is_divisible_by_100_but_not_by_400(self):
-        assert TestAYearIsALeapYear.checker.is_leap_year(1900) == False
+    @pytest.mark.parametrize("year", [2100, 1900, 100])
+    def test_if_it_is_divisible_by_100_but_not_by_400(self, year):
+        assert TestAYearIsALeapYear.checker.is_leap_year(year) == False
